Farmer-Markus/gnome-control-center

commit 52da4dafeb87af85d7bea00449d73aac1d97be72

info: Move duplicate space removal to info-cleanup.c
So that we don't have to do that separately for CPUs.

https://bugzilla.gnome.org/show_bug.cgi?id=774240

panels/info/info-cleanup.c
@@ -88,8 +88,47 @@ prettify_info (const char *info)
   return pretty;
 }
 
+static char *
+remove_duplicate_whitespace (const char *old)
+{
+  char   *new;
+  GRegex *re;
+  GError *error;
+
+  error = NULL;
+  re = g_regex_new ("[ \t\n\r]+", G_REGEX_MULTILINE, 0, &error);
+  if (re == NULL)
+    {
+      g_warning ("Error building regex: %s", error->message);
+      g_error_free (error);
+      return g_strdup (old);
+    }
+  new = g_regex_replace (re,
+                         old,
+                         -1,
+                         0,
+                         " ",
+                         0,
+                         &error);
+  g_regex_unref (re);
+  if (new == NULL)
+    {
+      g_warning ("Error replacing string: %s", error->message);
+      g_error_free (error);
+      return g_strdup (old);
+    }
+
+  return new;
+}
+
 char *
 info_cleanup (const char *input)
 {
-  return prettify_info (input);
+  char *pretty, *ret;
+
+  pretty = prettify_info (input);
+  ret = remove_duplicate_whitespace (pretty);
+  g_free (pretty);
+
+  return ret;
 }

panels/info/cc-info-panel.c
@@ -619,40 +619,6 @@ get_primary_disc_info (CcInfoPanel *self)
 }
 
 static char *
-remove_duplicate_whitespace (const char *old)
-{
-  char   *new;
-  GRegex *re;
-  GError *error;
-
-  error = NULL;
-  re = g_regex_new ("[ \t\n\r]+", G_REGEX_MULTILINE, 0, &error);
-  if (re == NULL)
-    {
-      g_warning ("Error building regex: %s", error->message);
-      g_error_free (error);
-      return g_strdup (old);
-    }
-  new = g_regex_replace (re,
-                         old,
-                         -1,
-                         0,
-                         " ",
-                         0,
-                         &error);
-  g_regex_unref (re);
-  if (new == NULL)
-    {
-      g_warning ("Error replacing string: %s", error->message);
-      g_error_free (error);
-      return g_strdup (old);
-    }
-
-  return new;
-}
-
-
-static char *
 get_cpu_info (const glibtop_sysinfo *info)
 {
   GHashTable    *counts;
@@ -694,22 +660,21 @@ get_cpu_info (const glibtop_sysinfo *info)
   g_hash_table_iter_init (&iter, counts);
   while (g_hash_table_iter_next (&iter, &key, &value))
     {
-      char *stripped;
+      char *cleanedup;
       int   count;
 
       count = GPOINTER_TO_INT (value);
-      stripped = remove_duplicate_whitespace ((const char *)key);
+      cleanedup = info_cleanup ((const char *) key);
       if (count > 1)
-        g_string_append_printf (cpu, "%s \303\227 %d ", stripped, count);
+        g_string_append_printf (cpu, "%s \303\227 %d ", cleanedup, count);
       else
-        g_string_append_printf (cpu, "%s ", stripped);
-      g_free (stripped);
+        g_string_append_printf (cpu, "%s ", cleanedup);
+      g_free (cleanedup);
     }
 
   g_hash_table_destroy (counts);
 
-  ret = info_cleanup (cpu->str);
-  g_string_free (cpu, TRUE);
+  ret = g_string_free (cpu, FALSE);
 
   return ret;
 }

commit c9bdbb62d4d31deca88a73279d64b5c095704c46

info: Do not crash if Renderer is not set by SessionManager
This regression has been introduced by commit 52da4da. The
info panel crashes if prettify_info() returns NULL. This happens
if Renderer property from SessionManager is empty.

https://bugzilla.gnome.org/show_bug.cgi?id=774240

panels/info/info-cleanup.c
@@ -99,6 +99,9 @@ remove_duplicate_whitespace (const char *old)
   GRegex *re;
   GError *error;
 
+  if (old == NULL)
+    return NULL;
+
   error = NULL;
   re = g_regex_new ("[ \t\n\r]+", G_REGEX_MULTILINE, 0, &error);
   if (re == NULL)