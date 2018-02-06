Farmer-Markus/gnome-control-center

commit 6b043fd3ddbd9306b677471a49835404d6be0ba6

network: Fix cloned MAC not being saved for Ethernet
We read the MAC as being the cloned MAC, which NM would likely take to
mean that we didn't want a cloned MAC.

See https://bugzilla.redhat.com/show_bug.cgi?id=1467295

https://bugzilla.gnome.org/show_bug.cgi?id=793072

panels/network/connection-editor/ce-page-ethernet.c
@@ -86,7 +86,7 @@ static void
 ui_to_setting (CEPageEthernet *page)
 {
         gchar *device_mac = NULL;
-        gchar *cloned_mac;
+        gchar *cloned_mac = NULL;
         const gchar *text;
         GtkWidget *entry;
 
@@ -95,8 +95,12 @@ ui_to_setting (CEPageEthernet *page)
                 text = gtk_entry_get_text (GTK_ENTRY (entry));
                 device_mac = ce_page_trim_address (text);
         }
-        text = gtk_entry_get_text (GTK_ENTRY (entry));
-        cloned_mac = ce_page_trim_address (text);
+
+        entry = gtk_bin_get_child (GTK_BIN (page->cloned_mac));
+        if (entry) {
+                text = gtk_entry_get_text (GTK_ENTRY (entry));
+                cloned_mac = ce_page_trim_address (text);
+        }
 
         g_object_set (page->setting_wired,
                       NM_SETTING_WIRED_MAC_ADDRESS, device_mac,

commit 5d7147ce66752e70f02629617bce4977e3a274d8

network: Really fix clone MAC support
This isn't a container, but a text entry. Bug introduced in commit
6b043fd.

panels/network/connection-editor/ce-page-ethernet.c
@@ -96,11 +96,8 @@ ui_to_setting (CEPageEthernet *page)
                 device_mac = ce_page_trim_address (text);
         }
 
-        entry = gtk_bin_get_child (GTK_BIN (page->cloned_mac));
-        if (entry) {
-                text = gtk_entry_get_text (GTK_ENTRY (entry));
-                cloned_mac = ce_page_trim_address (text);
-        }
+        text = gtk_entry_get_text (GTK_ENTRY (entry));
+        cloned_mac = ce_page_trim_address (text);
 
         g_object_set (page->setting_wired,
                       NM_SETTING_WIRED_MAC_ADDRESS, device_mac,