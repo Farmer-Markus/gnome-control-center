Farmer-Markus/gnome-control-center

commit ee53bfd8b6bbbd018fdd7a2b1ea33e5a44a1b43b

network: Fixed Network profiles shown on wrong device
Added missing interface name during network profile creation.

Fixes #353

Fixed issues from code review

panels/network/net-device-ethernet.c
@@ -395,8 +395,10 @@ add_profile_button_clicked_cb (NetDeviceEthernet *self)
         NMSettingConnection *sc;
         g_autofree gchar *uuid = NULL;
         g_autofree gchar *id = NULL;
+        g_autoptr(GError) error = NULL;
         NetConnectionEditor *editor;
         const GPtrArray *connections;
+        const char *iface;
 
         connection = nm_simple_connection_new ();
         sc = NM_SETTING_CONNECTION (nm_setting_connection_new ());
@@ -413,6 +415,16 @@ add_profile_button_clicked_cb (NetDeviceEthernet *self)
                       NM_SETTING_CONNECTION_TYPE, NM_SETTING_WIRED_SETTING_NAME,
                       NM_SETTING_CONNECTION_AUTOCONNECT, TRUE,
                       NULL);
+
+        iface = nm_device_get_iface (self->device);
+        if (nm_utils_is_valid_iface_name (iface, &error)) {
+            g_object_set (sc,
+                          NM_SETTING_CONNECTION_INTERFACE_NAME, iface,
+                          NULL);
+        } else {
+            g_warning ("Invalid interface Name '%s': %s", iface, error->message);
+            g_error_free (error);
+        }
 
         nm_connection_add_setting (connection, nm_setting_wired_new ());
 

commit 81d1b7119a1b410adb61ba2d4fb82f445f6bb9a0

net-device-ethernet: Prevent double free
Fixes: ee53bfd8b6bbbd018fdd7a2b1ea33e5a44a1b43b

panels/network/net-device-ethernet.c
@@ -423,7 +423,6 @@ add_profile_button_clicked_cb (NetDeviceEthernet *self)
                           NULL);
         } else {
             g_warning ("Invalid interface Name '%s': %s", iface, error->message);
-            g_error_free (error);
         }
 
         nm_connection_add_setting (connection, nm_setting_wired_new ());