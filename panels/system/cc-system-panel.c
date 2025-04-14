067782427f470dd77a5a8b15ef996a267b86c8ca
Farmer_Markus <145863173+Farmer-Markus@users.noreply.github.com>
2025-04-14 14:49:50 +0200

    testing

--- panels/system/cc-system-panel.c
+++ panels/system/cc-system-panel.c
@@ -23,6 +23,14 @@
 
 #include <glib/gi18n-lib.h>
 
+
+
+#include <glib/gi18n.h>
+#include "cc-system-details-window.h"
+#include "cc-hostname-entry.h"
+
+
+
 #include "cc-list-row.h"
 #include "cc-system-panel.h"
 #include "cc-system-resources.h"
@@ -47,9 +55,155 @@ struct _CcSystemPanel
 
   CcSecureShellPage *secure_shell_dialog;
   AdwNavigationPage *software_updates_group;
+
+
+  AdwActionRow    *disk_row;
+  AdwActionRow    *hardware_model_row;
+  AdwActionRow    *memory_row;
+  GtkPicture      *os_logo;
+  AdwActionRow    *os_name_row;
+  AdwActionRow    *processor_row;
+
+  AdwDialog       *system_details_window;
+  guint            create_system_details_id;
 };
 
 CC_PANEL_REGISTER (CcSystemPanel, cc_system_panel)
+
+
+
+
+
+
+G_DEFINE_TYPE (CcSystemPanel, cc_system_panel, ADW_TYPE_NAVIGATION_PAGE)
+
+
+static void
+system_about_page_setup_overview (CcSystemPanel *self)
+{
+  guint64 ram_size;
+  g_autofree char *memory_text = NULL;
+  g_autofree char *cpu_text = NULL;
+  g_autofree char *os_name_text = NULL;
+  g_autofree char *hardware_model_text = NULL;
+  g_autofree gchar *disk_capacity_string = NULL;
+
+  hardware_model_text = get_hardware_model_string ();
+  adw_action_row_set_subtitle (self->hardware_model_row, hardware_model_text);
+  gtk_widget_set_visible (GTK_WIDGET (self->hardware_model_row), hardware_model_text != NULL);
+
+  ram_size = get_ram_size_dmi ();
+  if (ram_size == 0)
+    ram_size = get_ram_size_libgtop ();
+  memory_text = g_format_size_full (ram_size, G_FORMAT_SIZE_IEC_UNITS);
+  adw_action_row_set_subtitle (self->memory_row, memory_text);
+
+  cpu_text = get_cpu_info ();
+  adw_action_row_set_subtitle (self->processor_row, cpu_text);
+
+  disk_capacity_string = get_primary_disk_info ();
+  if (disk_capacity_string == NULL)
+    disk_capacity_string = g_strdup (_("Unknown"));
+  adw_action_row_set_subtitle (self->disk_row, disk_capacity_string);
+
+  os_name_text = get_os_name ();
+  adw_action_row_set_subtitle (self->os_name_row, os_name_text);
+}
+
+static gboolean
+cc_system_about_page_create_system_details (CcSystemPanel *self)
+{
+  if (!self->system_details_window)
+    {
+      self->system_details_window = ADW_DIALOG (cc_system_details_window_new ());
+      g_object_ref_sink (self->system_details_window);
+    }
+
+  g_clear_handle_id (&self->create_system_details_id, g_source_remove);
+
+  return G_SOURCE_REMOVE;
+}
+
+static void
+cc_system_about_page_open_system_details (CcSystemPanel *self)
+{
+  cc_system_about_page_create_system_details (self);
+
+  adw_dialog_present (self->system_details_window, GTK_WIDGET (self));
+}
+
+#if !defined(DISTRIBUTOR_LOGO) || defined(DARK_MODE_DISTRIBUTOR_LOGO)
+static gboolean
+use_dark_theme (CcSystemPanel *self)
+{
+  AdwStyleManager *style_manager = adw_style_manager_get_default ();
+
+  return adw_style_manager_get_dark (style_manager);
+}
+#endif
+
+tatic void
+setup_os_logo (CcSystemPanel *self)
+{
+#ifdef DISTRIBUTOR_LOGO
+#ifdef DARK_MODE_DISTRIBUTOR_LOGO
+  if (use_dark_theme (self))
+    {
+      gtk_picture_set_filename (self->os_logo, DARK_MODE_DISTRIBUTOR_LOGO);
+      return;
+    }
+#endif
+  gtk_picture_set_filename (self->os_logo, DISTRIBUTOR_LOGO);
+  return;
+#else
+  GtkIconTheme *icon_theme;
+  g_autofree char *logo_name = g_get_os_info ("LOGO");
+  g_autoptr(GtkIconPaintable) icon_paintable = NULL;
+  g_autoptr(GPtrArray) array = NULL;
+  g_autoptr(GIcon) icon = NULL;
+  gboolean dark;
+
+  dark = use_dark_theme (self);
+  if (logo_name == NULL)
+    logo_name = g_strdup ("gnome-logo");
+
+  array = g_ptr_array_new_with_free_func (g_free);
+  if (dark)
+    g_ptr_array_add (array, (gpointer) g_strdup_printf ("%s-text-dark", logo_name));
+  g_ptr_array_add (array, (gpointer) g_strdup_printf ("%s-text", logo_name));
+  if (dark)
+    g_ptr_array_add (array, (gpointer) g_strdup_printf ("%s-dark", logo_name));
+  g_ptr_array_add (array, (gpointer) g_strdup_printf ("%s", logo_name));
+
+  icon = g_themed_icon_new_from_names ((char **) array->pdata, array->len);
+  icon_theme = gtk_icon_theme_get_for_display (gdk_display_get_default ());
+  icon_paintable = gtk_icon_theme_lookup_by_gicon (icon_theme, icon,
+                                                   192,
+                                                   gtk_widget_get_scale_factor (GTK_WIDGET (self)),
+                                                   gtk_widget_get_direction (GTK_WIDGET (self)),
+                                                   0);
+  gtk_picture_set_paintable (self->os_logo, GDK_PAINTABLE (icon_paintable));
+#endif
+}
+
+static void
+cc_about_page_dispose (GObject *object)
+{
+  CcAboutPage *self = CC_SYSTEM_PANEL (object);
+
+  if (self->system_details_window)
+    adw_dialog_close (self->system_details_window);
+  g_clear_object (&self->system_details_window);
+
+  g_clear_handle_id (&self->create_system_details_id, g_source_remove);
+
+  G_OBJECT_CLASS (cc_about_page_parent_class)->dispose (object);
+}
+
+
+
+
+
 
 static gboolean
 gnome_software_allows_updates (void)
